bd16e6382c82dae5777e27c2c994aba130bce941
garobcsi <gasparrobert0908@gmail.com>
2023-02-04 17:00:03 +0100

    Migration & Model

---
+++ database/migrations/2023_02_04_121449_create_pandas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pandas', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',10);
+            $table->string('sex',1);
+            $table->date('birth');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pandas');
+    }
+};
