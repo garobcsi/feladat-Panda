84d5633be2d800137ff5347361d77447474d7996
garobcsi <gasparrobert0908@gmail.com>
2023-02-04 16:59:32 +0100

    api routes made

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PandaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('pandas',[PandaController::class,'index'])->name('pandas.index');
+Route::get('pandas/{id}',[PandaController::class,'show'])->whereNumber('id')->name('pandas.show');
+Route::post('pandas',[PandaController::class,'store'])->name('pandas.store');
+Route::put('pandas/{id}',[PandaController::class,'update'])->whereNumber('id')->name('pandas.update');
+Route::delete('pandas/{id}',[PandaController::class,'destroy'])->whereNumber('id')->name('pandas.destroy');
