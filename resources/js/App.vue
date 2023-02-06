60dd2f07ee0066f47b95d784a65c958154592fcb
garobcsi <gasparrobert0908@gmail.com>
2023-02-06 10:56:07 +0100

    Órai munka (lehet hogy nem fog menni)

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,3 +1,12 @@
 <template>
-  <h1 class="">Helló, Vite és Vue meg Laravel!</h1>
+  <h1>Helló, Vite és Vue meg Laravel!</h1>
+    <Menu></Menu>
+    <Table></Table>
 </template>
+<script>
+import Menu from "./components/menu";
+import Table from "./components/table";
+export default {
+    components: {Table, Menu}
+}
+</script>
--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default ({ mode }) => {
     return defineConfig({
         plugins: [
             laravel({
-                input: ['resources/css/app.scss', 'resources/js/app.js'],
+                input: ['resources/css/app.scss', 'resources/js/*'],
                 refresh: true,
             }),
             vue({
@@ -26,6 +26,7 @@ export default ({ mode }) => {
             hmr: {
                 host: "localhost",
             }
+
         }
     });
 }
